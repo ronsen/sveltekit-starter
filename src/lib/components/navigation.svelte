d3e66b85b4b0b7c0df7967f6b51d00132ac59642
Ronsen <ronsen@startrek.or.id>
2023-01-14 10:09:34 +0700

    update

--- src/lib/components/navigation.svelte
+++ src/lib/components/navigation.svelte
@@ -8,7 +8,7 @@
     {#if prevPage > 0}
         <a
             href="{href}page={prevPage}"
-            class="border border-gray-200 p-2 text-gray-700 hover:text-gray-900"
+            class="border border-gray-200 px-4 py-2 text-gray-700 hover:text-gray-900 rounded"
             ><i class="bi bi-arrow-left" /></a
         >
     {/if}
@@ -16,7 +16,7 @@
     {#if nextPage > 0}
         <a
             href="{href}page={nextPage}"
-            class="border border-gray-200 p-2 text-gray-500 hover:text-gray-900"
+            class="border border-gray-200 px-4 py-2 text-gray-500 hover:text-gray-900 rounded"
             ><i class="bi bi-arrow-right" /></a
         >
     {/if}
