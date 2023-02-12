f02b6e8e5a480ff946f852ede3a73cbab8699442
Ronsen <ronsen@startrek.or.id>
2023-02-12 15:01:09 +0700

    saving tags on create

--- src/routes/(posts)/add/+page.server.ts
+++ src/routes/(posts)/add/+page.server.ts
@@ -13,13 +13,44 @@ export const actions: Actions = {
                 message: '<strong>Title</strong> can not be blank.'
             });
         }
+
+        const ids = [];
+
+        if (tagcsv) {
+            const tagNames = tagcsv.split(',');
+
+            const tags = tagNames.map(async (tagName) => {
+                const name = tagName.trim().toLowerCase();
+                const slug = slugify(tagName);
+
+                let tag = await db.tag.findFirst({
+                    where: { slug: slug }
+                });
+
+                if (!tag) {
+                    tag = await db.tag.create({
+                        data: { name, slug }
+                    });
+                }
+
+                return tag;
+            });
+
+            let i = 0;
+            for (const tag of tags) {
+                ids[i++] = { id: (await tag)?.id }
+            }
+        }
         
         const post = await db.post.create({
             data: {
                 title: title.trim(),
                 slug: slugify(title.trim().toLowerCase()),
                 content: content.trim(),
-                authorId: locals.user.id
+                authorId: locals.user.id,
+                tags: {
+                    connect: [...ids]
+                }
             }
         });
 
