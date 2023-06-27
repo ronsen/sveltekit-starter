1012942160d3f17f2d5e47bf3d6bd87ccece8b2e
Ronsen <ronsen@startrek.or.id>
2023-06-27 12:40:58 +0700

    update

--- src/lib/components/search.svelte
+++ src/lib/components/search.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
-    export let q: String = '';
+    export let q: string = '';
 </script>
 
 <div class="mb-6">
     <form method="GET" action="/search">
         <div class="form-control">
-            <div class="input-group">
-                <input type="text" name="q" value="{q}" class="input input-bordered w-full" required>
-                <button type="submit" class="btn btn-primary">Search</button>
+            <div class="join join-horizontal">
+                <input type="text" name="q" value="{q}" class="input input-bordered w-full join-item" required>
+                <button type="submit" class="btn btn-primary join-item">Search</button>
             </div>
         </div>
     </form>
