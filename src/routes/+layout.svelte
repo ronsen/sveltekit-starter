48c21f21092c3103b8c9103a16f1107bf79691d9
Ronsen <ronsen@startrek.or.id>
2023-09-26 09:24:24 +0700

    update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,36 +14,38 @@
     }
 </script>
 
-<div class="flex justify-between items-center border-b border-primary pb-2 mb-8">
-    <h1 class="font-bold uppercase"><a href="/">Catatan</a></h1>
+<div class="container px-8 md:w-3/4 mx-auto my-8" >
+    <div class="flex justify-between items-center border-b border-primary pb-2 mb-8">
+        <h1 class="font-bold uppercase"><a href="/">Catatan</a></h1>
 
-    <div class="inline-flex items-center gap-3">
-        <form method="post" class="form-control" use:enhance={updateTheme}>
-            <div class="dropdown dropdown-end">
-                <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-                <!-- svelte-ignore a11y-label-has-associated-control -->
-                <label tabindex="0" class="btn btn-sm btn-ghost rounded-btn"><i class="bi bi-sun"></i></label>
-                <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-                <ul tabindex="0" class="menu dropdown-content p-2 shadow bg-base-200 rounded-box w-52 mt-4">
-                    <li><button formaction="/?theme=light&redirectTo={$page.url.pathname}">Light</button></li> 
-                    <li><button formaction="/?theme=dark&redirectTo={$page.url.pathname}">Dark</button></li> 
-                </ul>
-            </div>
-        </form>
-        
-        {#if $page.data.user}
-            <a href="/add" title="Add New Note"><i class="bi bi-plus-circle"></i></a>
-            <a href="/settings" title="Settings"><i class="bi bi-gear"></i></a>
-        {/if}
-
-        {#if !$page.data.user}
-            <a href="/login" title="Sign In"><i class="bi bi-box-arrow-in-right"></i></a>
-        {:else}
-            <form method="POST" action="/logout">
-                <button type="submit" title="Sign Out"><i class="bi bi-box-arrow-right"></i></button>
+        <div class="inline-flex items-center gap-3">
+            <form method="post" class="form-control" use:enhance={updateTheme}>
+                <div class="dropdown dropdown-end">
+                    <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+                    <!-- svelte-ignore a11y-label-has-associated-control -->
+                    <label tabindex="0" class="btn btn-sm btn-ghost rounded-btn"><i class="bi bi-sun"></i></label>
+                    <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+                    <ul tabindex="0" class="menu dropdown-content p-2 shadow bg-base-200 rounded-box w-52 mt-4">
+                        <li><button formaction="/?theme=light&redirectTo={$page.url.pathname}">Light</button></li> 
+                        <li><button formaction="/?theme=dark&redirectTo={$page.url.pathname}">Dark</button></li> 
+                    </ul>
+                </div>
             </form>
-        {/if}
-    </div>
-</div>
+            
+            {#if $page.data.user}
+                <a href="/add" title="Add New Note"><i class="bi bi-plus-circle"></i></a>
+                <a href="/settings" title="Settings"><i class="bi bi-gear"></i></a>
+            {/if}
 
-<slot />
+            {#if !$page.data.user}
+                <a href="/login" title="Sign In"><i class="bi bi-box-arrow-in-right"></i></a>
+            {:else}
+                <form method="POST" action="/logout">
+                    <button type="submit" title="Sign Out"><i class="bi bi-box-arrow-right"></i></button>
+                </form>
+            {/if}
+        </div>
+    </div>
+
+    <slot />
+</div>
