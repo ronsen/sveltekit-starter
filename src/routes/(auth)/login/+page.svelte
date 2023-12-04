9639a07489fea4d693a4affa678410bd15c95171
Ronald Nababan <ronsen@startrek.or.id>
2023-12-04 11:25:41 +0700

    update

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -14,17 +14,15 @@
     <Alert>{@html form?.message}</Alert>
 {/if}
 
-<div class="w-full md:w-2/3 md:mx-auto">
-    <form method="post" use:enhance>
-        <div class="mb-3">
-            <input type="text" name="username" placeholder="Username" class="input input-bordered w-full">
-        </div>
-        <div class="mb-3">
-            <input type="password" name="password" placeholder="Password"  class="input input-bordered w-full">
-        </div>
-        <div class="inline-flex items-center gap-3">
-            <button type="submit" class="btn btn-primary">Sign In</button>
-            <a href="/register">Register</a>
-        </div>
-    </form>
-</div>
+<form method="post" use:enhance>
+	<div class="mb-3">
+		<input type="text" name="username" placeholder="Username" class="input input-bordered w-full">
+	</div>
+	<div class="mb-3">
+		<input type="password" name="password" placeholder="Password"  class="input input-bordered w-full">
+	</div>
+	<div class="inline-flex items-center gap-3">
+		<button type="submit" class="btn btn-primary">Sign In</button>
+		<a href="/register">Register</a>
+	</div>
+</form>
--- src/routes/(auth)/register/+page.svelte
+++ src/routes/(auth)/register/+page.svelte
@@ -14,16 +14,14 @@
     <Alert>{@html form?.message}</Alert>
 {/if}
 
-<div class="w-full md:w-2/3 md:mx-auto">
-    <form method="post" use:enhance>
-        <div class="mb-3">
-            <input type="text" name="username" placeholder="Username" class="input input-bordered w-full">
-        </div>
-        <div class="mb-3">
-            <input type="password" name="password" placeholder="Password"  class="input input-bordered w-full">
-        </div>
-        <div class="inline-flex items-center gap-3">
-            <button type="submit" class="btn btn-primary">Register</button>
-        </div>
-    </form>
-</div>
+<form method="post" use:enhance>
+	<div class="mb-3">
+		<input type="text" name="username" placeholder="Username" class="input input-bordered w-full">
+	</div>
+	<div class="mb-3">
+		<input type="password" name="password" placeholder="Password"  class="input input-bordered w-full">
+	</div>
+	<div class="inline-flex items-center gap-3">
+		<button type="submit" class="btn btn-primary">Register</button>
+	</div>
+</form>
