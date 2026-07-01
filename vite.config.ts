8f18f8479b2426128fd2171867ceeb3381a19c63
Ronsen <ronsen@startrek.or.id>
2026-07-01 13:22:43 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,14 @@ export default defineConfig({
 			// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 			// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-			adapter: adapter()
+			adapter: adapter(),
+
+			typescript: {
+				config: (config) => ({
+					...config,
+					include: [...config.include, '../prisma.config.ts']
+				})
+			}
 		})
 	]
 });
